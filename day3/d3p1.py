b20da59e01a6fcad5a7d5836ec3b13ee0ff23ff9
jed deitrick <jdeitrick80@jed-us.deitrick.pro>
2020-12-03 17:09:10 +0000

    day 3

--- day3/d3p1.py
+++ day3/d3p1.py
@@ -1,6 +1,19 @@
 with open('d3p1.in') as f:
-	lines = f.read().splitlines()
+    lines = f.read().splitlines()
+
+xmax = len(lines[0])
+ymax = len(lines)
+
+x = 0
+y = 0
+trees = 0
+
+while y < ymax:
+    if (lines[y].count('#', x, x+1)):
+        trees += 1
+    x = (x + 3) % xmax
+    y += 1
+
+print(trees)
 
 
-print("length of row: ", len(lines[0]))
-print("number of rows: ", len(lines))
