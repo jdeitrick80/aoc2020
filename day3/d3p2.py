641c4cbb8e55123e20485f016577f82a971d8725
jdeitrick80 <jed.deitrick@gmail.com>
2020-12-04 09:36:57 +0100

    day 3

--- day3/d3p2.py
+++ day3/d3p2.py
@@ -1,23 +1,24 @@
 with open('d3p1.in') as f:
-    lines = f.read().splitlines()
+	lines = f.read().splitlines()
 
 xmax = len(lines[0])
 ymax = len(lines)
 
-x = 0
-y = 0
 product = 1
 
-slopes = [3,1],
+slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]]
 
-trees = 0
+for row in slopes: 
+	x = 0
+	y = 0
+	trees = 0
+	while y < ymax:
+		if (lines[y].count('#', x, x+1)):
+			trees += 1
+		x = (x + row[0]) % xmax
+		y += row[1]
 
-while y < ymax:
-    if (lines[y].count('#', x, x+1)):
-        trees += 1
-    x = (x + 3) % xmax
-    y += 1
+	print(trees)
+	product *= trees
 
-print(trees)
-
-
+print(product)
