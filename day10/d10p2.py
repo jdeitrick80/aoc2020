0c559e43f5c0386f8837aacb25317f83617a033a
jdeitrick80 <jed.deitrick@gmail.com>
2020-12-10 18:10:34 +0100

    Day 10 update

--- day10/d10p2.py
+++ day10/d10p2.py
@@ -9,6 +9,8 @@ prev=0
 #if consecutive1s==2 combinations are 2
 #if consecutive1s==3 combinations are 4
 #if consecutive1s==4 combinations are 7
+#if consecutive1s==n combinations are n(n-1)/2+1
+
 consecutive1s=0
 cadapt=[0]
 combinations=1
@@ -16,25 +18,17 @@ combinations=1
 for adaptor in adaptors:
     if((adaptor-prev)==3):
         diff3+=1
-        if(consecutive1s>1 and consecutive1s<4):
-            combinations*=2*(consecutive1s-1)
-        elif(consecutive1s==4):
-            combinations*=7
+        combinations*=consecutive1s*(consecutive1s-1)/2+1
         consecutive1s=0
         cadapt=[adaptor]
     elif((adaptor-prev)==1):
         diff1+=1
         consecutive1s+=1
         cadapt.append(adaptor)
-    else:
-        print("Jump that is not 1 or 3")
     prev=adaptor
-if(consecutive1s>1 and consecutive1s<4):
-    combinations*=2*(consecutive1s-1)
-elif(consecutive1s==4):
-    combinations*=7
+combinations*=consecutive1s*(consecutive1s-1)/2+1
 diff3+=1
 
 total = diff1*diff3
 print(f"length:{len(adaptors)}")
-print(f"1:{diff1} 3:{diff3} total:{diff1*diff3} combinations:{combinations}")
+print(f"1:{diff1} 3:{diff3} total:{diff1*diff3} combinations:{int(combinations)}")
